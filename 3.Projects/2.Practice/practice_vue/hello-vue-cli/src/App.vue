9de662925ac60b71842cd9875e7a27cf68031cf8
chaboram <ckqhfka4520@naver.com>
2021-07-03 17:15:57 +0900

    Start project

--- 3.Projects/2.Practice/practice_vue/hello-vue-cli/src/App.vue
+++ 3.Projects/2.Practice/practice_vue/hello-vue-cli/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app">
+  <div id="app-wrapper">
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import appHeader from "./components/app-header.vue";
 export default {
   name: "App",
-  components: {},
+  components: { appHeader },
 };
 </script>
 
-<style></style>
+<style scoped>
+#app-wrapper {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ 3.Projects/2.Practice/practice_vue/hello-vue-cli/src/components/app-header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav-bar">
+    <div id="header-logo">
+      <h1>차보람</h1>
+    </div>
+    <ul id="header-menu">
+      <li class="nav-button">Vue 실습 목록1</li>
+      <li class="nav-button">Vue 실습 목록2</li>
+    </ul>
+    <ul>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#nav-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5em 1em;
+}
+</style>
