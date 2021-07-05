1885125430430489004862de465a731c7641bba5
chaboram <ckqhfka4520@naver.com>
2021-07-05 12:31:45 +0900

    Delete

--- 3.Projects/2.Practice/practice_vue/hello-vue-cli/src/router/index.js
+++ 3.Projects/2.Practice/practice_vue/hello-vue-cli/src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from "vue-router";
 import slotParent from "../views/slotParent";
 import exDashBoard from "../views/exampleDashBoard";
 import animationWithWatch1 from "../components/examples/animationWithWatch1";
-import animationWithWatch2 from "../components/examples/animationWithWatch2";
 
 Vue.use(VueRouter);
 const routes = [
@@ -13,11 +12,6 @@ const routes = [
     path: "/animationWithWatch1",
     name: "animation-with-watch1",
     component: animationWithWatch1,
-  },
-  {
-    path: "/animationWithWatch2",
-    name: "animation-with-watch2",
-    component: animationWithWatch2,
   },
 ];
 const router = new VueRouter({
